4bb14626ac7af069c9332f17916a194b20f6e5cd
Xander Moffatt <xander.moffatt@gmail.com>
2016-09-12 13:33:32 -0600

    Created server and model packages, and stubbed ServerProxy classes. Testing commiting from my computer.

---
+++ java/src/client/server/IServerProxy.java
@@ -0,0 +1,9 @@
+package client.server;
+
+/**
+ * Interface for the Server proxy, and the mock proxy.
+ * Contains all server API methods.
+ * Created by Xander on 9/12/2016.
+ */
+public interface IServerProxy {
+}
---
+++ java/src/client/server/ServerProxy.java
@@ -0,0 +1,8 @@
+package client.server;
+
+/**
+ * Communicates with the server API.
+ * Created by Xander on 9/12/2016.
+ */
+public class ServerProxy implements IServerProxy {
+}
