6f97c62d67f4e67b7188ad2bf3f6be66a8d09992
jerekeith2 <jeremy.m.keith@gmail.com>
2016-09-14 13:39:05 -0600

    added javadoc to Map class

--- java/src/client/model/Map.java
+++ java/src/client/model/Map.java
@@ -1,7 +1,48 @@
 package client.model;
 
+import java.util.List;
+
 /**
- * Created by Jonathan Skaggs on 9/14/2016.
+ * Map class.
  */
 public class Map {
+	
+	private List<Hex> hexList;
+	
+	/**
+	 * Checks whether the map is able to add a city.
+	 * @param location the location of the city to place.
+	 * @return
+	 */
+	public boolean canAddCity(MapLocation location) {
+		return false;
+	}
+	
+	/**
+	 * Checks whether the map is able to add a settlement.
+	 * @param location the location of the settlement to place.
+	 * @return
+	 */
+	public boolean canAddSettlement(MapLocation location) {
+		return false;
+	}
+	
+	/**
+	 * Checks whether the map is able to add a road.
+	 * @param location the location of the road to place.
+	 * @return
+	 */
+	public boolean canAddRoad(MapLocation location) {
+		return false;
+	}
+	
+	/**
+	 * Checks whether there are resources to be offered to a player.
+	 * @param diceRoll the value rolled by the dice.
+	 * @param playerId the playerId of the player looking for resources around the rolled value.
+	 * @return
+	 */
+	public boolean canGetRolledResourses(int diceRoll, int playerId ) {
+		return false;
+	}
 }
