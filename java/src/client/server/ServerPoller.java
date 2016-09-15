511903836b2bee619efea0d72d343b77f2f3ad6d
Spencer Olsen <spencer.p.olsen@gmail.com>
2016-09-14 21:24:00 -0600

    Updates to ServerPoller method headers and start of implementation on the ServerPoller. Added a return type to gameGetModel on the IServerProxy.

--- java/src/client/server/ServerPoller.java
+++ java/src/client/server/ServerPoller.java
@@ -1,10 +1,12 @@
 package client.server;
 
+import org.json.simple.JSONObject;
 
 /**
  * @author	Spencer Olsen
  * 
- * Polls the server to check whether a new model exists.
+ * Polls the server to check whether a new model exists. If a new model exists, the ServerPoller sends
+ * the new data to the Model to update itself.
  */
 
 public class ServerPoller {
@@ -19,7 +21,7 @@ public class ServerPoller {
 	 * The version of the server since the last poll. 
 	 * Used to check if changes have been made to the server
 	 * by comparing the version numbers.
-	 * <p>
+	 * </p>
 	 */
 	private int version;
 	
@@ -37,23 +39,69 @@ public class ServerPoller {
 	 * @param version The version of the server since the last poll
 	 * @param proxy The proxy to use when polling the server.
 	 */
-	public ServerPoller(int interval, int version, IServerProxy proxy) {
-		
-	}
+	public ServerPoller(int interval, int version, IServerProxy proxy);
 	
 	/**
 	 * Calls the server proxy to retrieve the current model state.
+	 *
+	 * @pre <pre>
+	 *     1. proxy is not null
+	 *     2. version is not null
+	 *
+	 * </pre>
+	 *
+	 * @post <pre>
+	 * 	If the operation succeeds:
+	 * 		1. A new version of the model is received from the server
+	 *
+	 * If the operation fails:
+	 * 		1. The server returns an HTTP 400 error message and the response body contains an error message
+	 * </pre>
 	 */
 	public void pollServer() {
-		
+		JSONObject result = proxy.getGameModel(version);
+		if (checkForUpdates(result)) {
+			// get new version number
+
+
+			updateModel(result);
+		}
+	}
+
+	/**
+	 * Checks if the JSONObject contains updated data.
+	 *
+	 * @post<pre>
+	 * If the JSONObject is null, the method returns false, signifying that there is no new data.
+	 * If the JSONObject is not null, the method returns true, signifying that there was new data
+	 * retrieved from the server.
+	 * </pre>
+	 *
+	 * @param data The JSONObject to check for new data
+	 * @return true if the JSONObject contains new data, otherwise false
+	 */
+	private boolean checkForUpdates(JSONObject data) {
+		if (data == null)
+			return false;
+		else
+			return true;
+
 	}
 	
 	/**
 	 * Sends the new JSON data to the Model so the Model can update itself.
+	 *
+	 * @pre <pre>
+	 * data is not null
+	 * </pre>
+	 *
+	 * @post <pre>
+	 * The model is updated with the new data.
+	 * </pre>
+	 *
+	 * @param data	The data to send to the model so it can update itself
 	 */
-	private void updateModel() {
-		
-	}
+	private void updateModel(JSONObject data) {}
 
 	public int getInterval() {
 		return interval;
