db52fca74a9ba4294cb6ce3eedc0c8fdf41a2c3e
Xander Moffatt <xander.moffatt@gmail.com>
2016-09-28 22:49:24 -0600

    Built 'ant test' target and made testing class with hello world. All tests will start in this class.

---
+++ java/src/testing/MainTest.java
@@ -0,0 +1,11 @@
+package testing;
+
+/**
+ * Main testing class. Every test needs to be started here.
+ */
+public class MainTest {
+
+    public static void main(final String[] args) {
+        System.out.print("Hello World.");
+    }
+}
