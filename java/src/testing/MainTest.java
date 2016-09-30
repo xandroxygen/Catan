0f542944442492a015422cf41b0c481ad5c791c5
Jonathan Skaggs <jskaggs93@gmail.com>
2016-09-30 12:26:51 -0600

    made the methods in the Model Public

--- java/src/testing/MainTest.java
+++ java/src/testing/MainTest.java
@@ -1,5 +1,10 @@
 package testing;
 
+import client.model.Model;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 /**
  * Main testing class. Every test needs to be started here.
  */
@@ -7,5 +12,10 @@ public class MainTest {
 
     public static void main(final String[] args) {
         System.out.print("Hello World.");
+    }
+
+    @Test
+    public void canBuyDevelopmentCard() throws Exception {
+        assertTrue(Model.getInstance().);
     }
 }
