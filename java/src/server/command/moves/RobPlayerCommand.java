92423d7f479cc31b243a05a03618cf78b1a27fe8
Xander Moffatt <xander.moffatt@gmail.com>
2016-11-16 22:53:12 -0700

    Fixed rob request/handler/command to make it work.

--- java/src/server/command/moves/RobPlayerCommand.java
+++ java/src/server/command/moves/RobPlayerCommand.java
@@ -8,9 +8,9 @@ import shared.model.InvalidActionException;
 public class RobPlayerCommand extends Command{
 
 	/**
-	 * the ID of the player doing the robbing
+	 * the Index of the player doing the robbing
 	 */
-	private int playerID;
+	private int playerIndex;
 	/**
 	 * the location to place the robber
 	 */
@@ -20,8 +20,9 @@ public class RobPlayerCommand extends Command{
 	 */
 	private int victimIndex;
 	
-	public RobPlayerCommand(IServerFacade facade, int gameID, int playerID, int victimIndex, HexLocation location) {
+	public RobPlayerCommand(IServerFacade facade, int gameID, int playerIndex, int victimIndex, HexLocation location) {
 		super(gameID, facade);
+		this.playerIndex = playerIndex;
 		this.location = location;
 		this.victimIndex = victimIndex;
 	}
@@ -35,8 +36,7 @@ public class RobPlayerCommand extends Command{
 	 * </pre>
 	 */
 	public Object execute() throws InvalidActionException{
-		//return this.getFacade().robPlayer(this.getGameID(), playerID, victimIndex, location);
-		return null;
+		return this.getFacade().robPlayer(this.getGameID(), playerIndex, location, victimIndex);
 	}
 
 }
